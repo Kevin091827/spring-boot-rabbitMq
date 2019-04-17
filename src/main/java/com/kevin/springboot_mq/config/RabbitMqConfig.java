14a41e5e66fbee8109a5c4394eba699d261a7fd4
kevin <1264222041@qq.com>
2019-04-17 21:05:45 +0800

    kevin fix a bug

--- src/main/java/com/kevin/springboot_mq/config/RabbitMqConfig.java
+++ src/main/java/com/kevin/springboot_mq/config/RabbitMqConfig.java
@@ -1,11 +1,21 @@
 package com.kevin.springboot_mq.config;
 
+import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
@@ -21,25 +31,86 @@ import javax.annotation.Resource;
  */
 @Configuration
 @Slf4j
-public class RabbitMqConfig  {
+public class RabbitMqConfig {
 
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
 
     /**
      * 定制rabbitMQ模板
-     *
+     * <p>
      * ConfirmCallback接口用于实现消息发送到RabbitMQ交换器后接收消息成功回调
      * ReturnCallback接口用于实现消息发送到RabbitMQ交换器，但无相应队列与交换器绑定时的回调  即消息发送不到任何一个队列中回调
+     *</p>
      * @return
      */
-    @PostConstruct
-    public void initRabbitTemplate(){
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
 
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setConnectionFactory(connectionFactory());
         //可针对每次请求的消息去确定’mandatory’的boolean值
         rabbitTemplate.setMandatory(true);
         rabbitTemplate.setConfirmCallback(new RabbitConfirmCallback());
         rabbitTemplate.setReturnCallback(new RabbitReturnCallback());
+        return rabbitTemplate;
     }
+
+    /**
+     * 连接工厂
+     * @return
+     */
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host, port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setPublisherConfirms(true);
+        return connectionFactory;
+    }
+
+    /**
+    @Bean
+    public SimpleMessageListenerContainer messageContainer() {
+        //加载处理消息A的队列
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory());
+        //设置接收多个队列里面的消息，这里设置接收队列A
+        //假如想一个消费者处理多个队列里面的信息可以如下设置：
+        //container.setQueues(queueA(),queueB(),queueC());
+        container.setQueues(queueA());
+        container.setExposeListenerChannel(true);
+        //设置最大的并发的消费者数量
+        container.setMaxConcurrentConsumers(10);
+        //最小的并发消费者的数量
+        container.setConcurrentConsumers(1);
+        //设置确认模式手工确认
+        container.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        container.setMessageListener(new ChannelAwareMessageListener() {
+            @Override
+            public void onMessage(Message message, Channel channel) throws Exception {
+                  //通过basic.qos方法设置prefetch_count=1，这样RabbitMQ就会使得每个Consumer在同一个时间点最多处理一个Message，
+                 //换句话说,在接收到该Consumer的ack前,它不会将新的Message分发给它
+                channel.basicQos(1);
+                byte[] body = message.getBody();
+                log.info("接收处理队列A当中的消息:" + new String(body));
+                //为了保证永远不会丢失消息，RabbitMQ支持消息应答机制。
+                // 当消费者接收到消息并完成任务后会往RabbitMQ服务器发送一条确认的命令，然后RabbitMQ才会将消息删除。
+                channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+            }
+        });
+        return container;
+    }
+    */
 
 }
